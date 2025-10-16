33803ac0d0ab94f227c70dbdff606ee8d4ba840c
seojun <asopip8709@gmail.com>
2025-10-16 11:43:41 +0900

    start or over

---
+++ Assets/script/Replay Script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayScript: MonoBehaviour
+{
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("game start");
+    }
+}
